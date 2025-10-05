fcf7856cdf409c8dc60032eefdfcb62c07656e45
pluslatte <debrisfield0201@gmail.com>
2025-10-05 15:03:14 +0900

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Container } from "@chakra-ui/react"
+import { Box, Container, Grid, GridItem, Text } from "@chakra-ui/react"
 import PerkList from "./components/PerkList"
 
 function App() {
   return (
-    <Container maxW="xl">
-      <PerkList />
+    <Container maxW="4xl">
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <GridItem colSpan={1}>
+          <Box p={4}>
+            <PerkList />
+          </Box>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text>Item Section</Text>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Text>Details</Text>
+        </GridItem>
+      </Grid>
     </Container>
   )
 }
