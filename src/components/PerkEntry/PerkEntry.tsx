aa8dd585ae09cba8cba00aa8fd36a40988310091
pluslatte <debrisfield0201@gmail.com>
2025-10-05 15:19:03 +0900

    tooltip

--- src/components/PerkEntry/PerkEntry.tsx
+++ src/components/PerkEntry/PerkEntry.tsx
@@ -1,20 +1,23 @@
 import type { Perk } from "@/lib/perks";
 import PerkIcon from "./PerkIcon";
 import { Box, Circle, Float } from "@chakra-ui/react";
+import { Tooltip } from "@/components/ui/tooltip";
 
 interface Props {
   perk: Perk;
 }
 const PerkEntry = (props: Props) => {
   return (
-    <Box position="relative">
-      <Float placement="bottom-center">
-        <Circle size="5" bg="gray.700" color="gray.300">
-          {props.perk.pp}
-        </Circle>
-      </Float>
-      <PerkIcon imgUrl={props.perk.iconUrl} />
-    </Box>
+    <Tooltip content={props.perk.name.en}>
+      <Box position="relative">
+        <Float placement="bottom-center">
+          <Circle size="5" bg="gray.700" color="gray.300">
+            {props.perk.pp}
+          </Circle>
+        </Float>
+        <PerkIcon imgUrl={props.perk.iconUrl} />
+      </Box>
+    </Tooltip>
   );
 };
 
